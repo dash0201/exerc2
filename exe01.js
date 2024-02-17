bf5c15ce73d93b3ede3a47b4825d1c0abe4965d0
Anna Luiza <160185227+dash0201@users.noreply.github.com>
2024-02-16 21:39:46 -0300

    Add files via upload

---
+++ exe01.js
@@ -0,0 +1,19 @@
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total)
+
+let nome = 'Karol 16';
+console.log(isNaN(nome))
+let nomes = 'Anna 17';
+console.log(isNaN(nome))
+
+let num = '200';
+console.log(+num + 50)
+
+let incremento = 5;
+console.log(++incremento)
+
+let numero = '80';
+let unidade = 'kg';
+let pesoPorDois = numero / 2;
+let peso = pesoPorDois + unidade;
+console.log(peso)
